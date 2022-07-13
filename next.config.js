e6a073e56f637e40f5635ee7ad64dc4390de6ed5
Trung Nguyen <trungnus96@gmail.com>
2022-07-13 23:23:25 +1000

    U1

--- next.config.js
+++ next.config.js
@@ -2,6 +2,27 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+  async headers() {
+    return [
+      {
+        source: "/hello",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*",
+          },
+          {
+            key: "Cross-Origin-Embedder-Policy",
+            value: "require-corp",
+          },
+          {
+            key: "Cross-Origin-Opener-Policy",
+            value: "same-origin",
+          },
+        ],
+      },
+    ];
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
